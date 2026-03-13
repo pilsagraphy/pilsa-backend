516ef10aca30cbe369534eaa6a5d54d7e3b6a994
psns <psns0122@naver.com>
2026-03-14 00:15:03 +0900

    ✨ fe 일정권한 확인 백추가

---
+++ src/main/java/com/back/global/role/dto/RoleResponse.java
@@ -0,0 +1,12 @@
+package com.back.global.role.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleResponse {
+  private String role;
+}
