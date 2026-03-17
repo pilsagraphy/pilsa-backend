0d2e35ce33e4ae41107b2a3f606b232981c55741
psns <psns0122@naver.com>
2026-03-18 02:12:53 +0900

    webConfig 수정

--- src/main/java/com/back/global/config/WebConfig.java
+++ src/main/java/com/back/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.back.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
   
+  @Value("${app.upload.dir}")
+  private String uploadDir;
+  
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    String path = uploadDir.endsWith("/") ? uploadDir : uploadDir + "/";
     registry.addResourceHandler("/uploads/**")
-        .addResourceLocations("file:./uploads/");
+        .addResourceLocations("file:" + path);
   }
 }
